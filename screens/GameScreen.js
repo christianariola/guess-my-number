3d647b2c8a180bebd13c852125b33e476fee5032
otakuchan <ianbjs@gmail.com>
2022-08-29 23:52:39 -0700

    Add Color.js constant for global colors

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import Title from '../components/Title'
 
 const GameScreen = () => {
-    return (
-    <div>
-        
-    </div>
-    )
+    return <>
+        <View style={styles.screen}>
+            <Title>Opponent's Guess</Title>
+            {/* GUESS */}
+            <View>
+                <Text>Higher or Lower?</Text>
+                {/* + - */}
+            </View>
+            {/* <View>LOG ROUNDS</View> */}
+        </View>
+    </>
 }
 
 export default GameScreen
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 28,
+        paddingTop: 36
+    }
+})
